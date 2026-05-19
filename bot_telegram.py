874f2e008649bfa51577225148a72419f3abd441
gabriel071977costa-gif <gabriel.071977.costa@gmail.com>
2026-05-19 19:29:50 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -18,7 +18,7 @@ from preguntas import es_pregunta
 from google import genai
 
 # --- IMPORTAMOS LA LÓGICA DE INVERSIÓN ---
-# from bot_binance import ciclo_diario, ejecutar_operacion
+from bot_binance import ciclo_diario, ejecutar_operacion
 from balance_diario import balance_diario, calcular_balance, balance_hoy
 
 # --- BUSCADOR INTELIGENTE DE CLAVES ---
