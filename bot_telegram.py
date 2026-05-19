c9a8a2150b18de1ff98273343dc3bc09493e3da1
gabriel071977costa-gif <gabriel.071977.costa@gmail.com>
2026-05-18 21:33:09 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -85,7 +85,7 @@ def chat(message):
         except Exception as e:
             try:
                 response = client.models.generate_content(
-                    model="gemini-1.5-flash", 
+                    model="gemini-2.0-flash", 
                     contents=message.text
                 )
                 bot.reply_to(message, response.text)
